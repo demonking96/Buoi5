db5603bb59ae6ecc0949ff19711e07839d590b85
Đức Nhân <avenged.king@gmail.com>
2017-11-05 23:26:05 +0700

    DNS

---
+++ Buoi5/DNSServer/DNSServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSServer
+{
+    internal class DNSServer
+    {
+        internal void startListening()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Buoi5/DNSServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSServer
+{
+    class Program
+    {
+        private static DNSServer named;
+        static void Main(string[] args)
+        {
+            named = new DNSServer();
+            while (!false)
+            {
+                named.startListening();
+            }
+        }
+    }
+}
